0b89d79b0e12087abbf97a963f8e4b934f11c623
VanillahHXZ <32707257+VanillahHXZ@users.noreply.github.com>
2019-03-24 21:12:24 +0800

    Add files via upload

---
+++ PrimeTest.java
@@ -0,0 +1,14 @@
+package softWareTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PrimeTest {
+
+	@Test
+	void test() {
+		PrintPrimes.printPrimes(1);
+	}
+
+}
